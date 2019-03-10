558852cf7b1f38c167b9cecf8520cbca9e16f9c3
Bujar <>
2019-03-10 15:05:56 -0400

    Added override and notes

--- assignment3/sortByTitle.java
+++ assignment3/sortByTitle.java
@@ -1,8 +1,12 @@
 import java.util.Comparator;
 
+
 public class sortByTitle implements Comparator<Item>{
 
+    @Override
     public int compare(Item firstItem, Item secondItem){
+        /*NOTE, item has its title sorted. Thus acess public method.
+        Secondly, uses string compareTo */
         return firstItem.getTitle().compareTo(secondItem.getTitle());
     }
 
